89f3d04ed7386414a824656eb334f019f07b2d9a
dimitrijd <dimitrijdugan@gmail.com>
2021-05-06 16:42:39 +0200

    fixed stack.sh]

--- scripts/stack.sh
+++ scripts/stack.sh
@@ -3,6 +3,12 @@
 # devcon --target: stack post-build init
 # appended to .bashrc and .zshrc
 
+# nvm environment
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] &&  \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# start mongod supervisord service
 alias mongod='start mongod'
 
 # ##
